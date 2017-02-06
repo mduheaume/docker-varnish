ac85773cd4260e0a7679436d3fe7b182c5e0046c
Mike du Heaume <mduheaume@gmail.com>
2017-02-06 09:32:38 -0700

    aggressive caching

--- run.sh
+++ run.sh
@@ -2,11 +2,28 @@
 
 echo "vcl 4.0;
 
-# Default backend definition. Set this to point to your content server.
 backend default {
     .host = \"`getent hosts ${VARNISH_BACKEND_HOST} | awk '{ print $1 }'`\";
     .port = \"${VARNISH_BACKEND_PORT}\";
+    .first_byte_timeout = 300s;
 }
+
+sub vcl_recv {
+     unset req.http.Cookie;
+}
+
+sub vcl_deliver {
+         if ( obj.hits > 0 )
+        {
+                set resp.http.X-Cache = \"HIT\";
+        }
+        else
+        {
+                set resp.http.X-Cache = \"MISS\";
+        }
+        set resp.http.X-Cache-Hits = obj.hits;
+}
+
 " > /etc/varnish/default.vcl
 
 varnishd -F -f /etc/varnish/default.vcl -s malloc,100M -a 0.0.0.0:${VARNISH_PORT}
